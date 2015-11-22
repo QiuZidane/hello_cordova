a733a05867b5256af6c25157c9a26a1c223a70be
QiuZidane <qiusdsdc@126.com>
2015-11-22 22:30:06 +0800

    2-ok

--- platforms/ios/www/js/test.js
+++ platforms/ios/www/js/test.js
@@ -3,4 +3,10 @@ function clickFunc(){
     alert('这里是iOS工作室');
 }
 var button1 = document.getElementById('button1');
-button1.addEventListener('click',clickFunc,false);
+if(button1) {
+    button1.addEventListener('click',clickFunc,false);
+}
+
+document.addEventListener("DOMContentLoaded",function(){
+    alert("We really do think that you are the best.")
+},false);
