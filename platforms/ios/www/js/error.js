93233928c498d96a85b3108a1e9208fa884d6142
QiuZidane <qiusdsdc@126.com>
2015-11-23 19:00:56 +0800

    3re

---
+++ platforms/ios/www/js/error.js
@@ -0,0 +1,8 @@
+window.onerror = function(msg, url, line) {  
+   var idx = url.lastIndexOf("/");  
+   if(idx > -1) {  
+    url = url.substring(idx+1);  
+   }  
+   alert("ERROR in " + url + " (line #" + line + "): " + msg);  
+   return false;  
+};
